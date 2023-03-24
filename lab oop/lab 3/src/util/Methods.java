d5b5c5b37f2a8c3710fbbccd6282c4271fff5a53
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-25 03:11:59 +0400

    changes

--- lab oop/lab 3/src/util/Methods.java
+++ lab oop/lab 3/src/util/Methods.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.Scanner;
+
 public class Methods {
 
     // 1.  Write a program that receives 10 numbers and prints the minimum even number
@@ -159,10 +161,85 @@ public class Methods {
         System.out.println(sum);
     }
 
+    // 16.  Write a program that accepts 10 numbers from the user and prints the sum of all the numbers that are higher than 10, smaller than 30 and even.
+    public static void task16(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            if (i < 30 && i > 10 && i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    // 17.  Write a program that accepts the weight of 40 students. Print the maximum weight under 50kg.
+    public static void task17(int[] arr) {
+        int max = 0;
+        for (int i : arr) {
+            if (i > max && i < 50) {
+                max = i;
+            }
+        }
+        System.out.println(max);
+    }
+
+    // 18.  Write a program that accepts a positive number and prints all the smaller positive numbers that can be divided by 2.
+    public static void task18(int a) {
+        for (int i = 0; i < a; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    // 19.  Write a program that accepts a number and prints all the smaller positive numbers that can't be divided by 2.
+    public static void task19(int a) {
+        for (int i = 0; i < a; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    // 23.  Write a program to find the sum of the digits of a natural number N.
+    public static int sumDigits(int a) {
+        int sum = 0;
+        while (a != 0) {
+            sum += a % 10;
+            a /= 10;
+        }
+        return sum;
+    }
+
+    // 24.  Write a program to find the number of digits of a natural number N.
+    public static int countDigits(int a) {
+        int count = 0;
+        while (a != 0) {
+            count++;
+            a /= 10;
+        }
+        return count;
+    }
+
+    // 25.  Write a program to determine the minimum of a given natural numbers
+    public static void task25() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of natural numbers: ");
+        int count = scanner.nextInt();
+
+        int[] numbers = new int[count];
+        for (int i = 0; i < count; i++) {
+            System.out.print("Enter a natural number: ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        int minimum = numbers[0];
+        for (int i = 1; i < count; i++) {
+            if (numbers[i] < minimum) {
+                minimum = numbers[i];
+            }
+        }
+        System.out.println(minimum);
+    }
+
 }
-
-
-
-
-
-
