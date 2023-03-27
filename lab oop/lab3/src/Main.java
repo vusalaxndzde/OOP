4b5e70447cd3107ac034ce82b09c737288caa857
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-27 23:02:58 +0400

    changes

--- lab oop/lab3/src/Main.java
+++ lab oop/lab3/src/Main.java
@@ -2,15 +2,7 @@ import static util.Methods.*;
 public class Main {
 
     public static void main(String[] args) {
-        printMaxEven(new int[]{1,2,3,8,4,5,6,7});
-
-        printMaxOdd(new int[]{1,2,3,9,4,5,6,7});
-
-        printMax(new int[]{1,2,3,9,4,5,6,7});
-
-        printMin(new int[]{1,2,3,9,4,5,6,7,0});
-
-        task5();
+        printMaxEven();
     }
 
 }
--- lab oop/lab3/src/util/Methods.java
+++ lab oop/lab3/src/util/Methods.java
@@ -5,33 +5,42 @@ import java.util.Scanner;
 public class Methods {
 
     // 1.  Write a program that receives 10 numbers and prints the minimum even number
-    public static void printMaxEven(int[] arr) {
+    public static void printMaxEven() {
+        Scanner scanner = new Scanner(System.in);
         int max = 0;
-        for (int i : arr) {
-            if (i % 2 == 0 && i > max) {
-                max = i;
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter number " + (i+1) + ": ");
+            int num = scanner.nextInt();
+            if (num % 2 == 0 && num > max) {
+                max = num;
             }
         }
         System.out.println(max);
     }
 
     // 2.  Write a program that receives 10 numbers and prints the maximum odd number
-    public static void printMaxOdd(int[] arr) {
+    public static void printMaxOdd() {
+        Scanner scanner = new Scanner(System.in);
         int max = 0;
-        for (int i : arr) {
-            if (!(i % 2 == 0) && i > max) {
-                max = i;
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter number " + (i+1) + ": ");
+            int num = scanner.nextInt();
+            if (num % 2 == 1 && num > max) {
+                max = num;
             }
         }
         System.out.println(max);
     }
 
     // 3.  Write a program that receives 10 numbers and prints the maximum
-    public static void printMax(int[] arr) {
+    public static void printMax() {
+        Scanner scanner = new Scanner(System.in);
         int max = 0;
-        for (int i : arr) {
-            if (i > max) {
-                max = i;
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter number " + (i+1) + ": ");
+            int num = scanner.nextInt();
+            if (num > max) {
+                max = num;
             }
         }
         System.out.println(max);
