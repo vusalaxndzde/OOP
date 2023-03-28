6e0c60bc316561db7882d638218e09ffa9ed4f08
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-29 02:34:02 +0400

    changes

--- lab oop/lab4/src/util/Methods.java
+++ lab oop/lab4/src/util/Methods.java
@@ -13,6 +13,44 @@ public class Methods {
         System.out.println(count);
     }
 
+    // Write a program that accepts 5 numbers in array. Print how many odd numbers there are.
+    public static void task2(int[] arr) {
+        int count = 0;
+        for (int i : arr) {
+            if (i % 2 == 1) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    // Write a program that accepts 10 numbers in array. Print only the even numbers.
+    public static void task3(int[] arr) {
+        for (int i : arr) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    // Write a program that accepts 10 numbers in array. Print only the elements in the even indexes. (0, 2, 4, 6…)
+    public static void task4(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
+    // Write a program that accepts 10 numbers in array. Print only the elements in the odd indexes. (1, 3, 5…)
+    public static void task5(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 1) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
     
 
 }
