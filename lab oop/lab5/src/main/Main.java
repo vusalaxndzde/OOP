ad994f3dc80ddab1671670a1494e4fb12207ec31
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-12 16:03:57 +0400

    changes

--- lab oop/lab5/src/main/Main.java
+++ lab oop/lab5/src/main/Main.java
@@ -1,10 +1,9 @@
 package main;
 
-import bean.Employee;
-import bean.Manager;
-import bean.Programmer;
+import bean.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Employee programmer = new Programmer(1, "Elon", "Musk", "backend developer", "p1");
         System.out.println(programmer);
@@ -12,4 +11,5 @@ public class Main {
         Employee manager = new Manager(1, "Steve", "Jobs", "manager at Apple", "m1");
         System.out.println(manager);
     }
+
 }
